20223fb7d8887f8706f4b82bea1b138fbdbc5417
Kevin Novak <kevinmichaelnovak@gmail.com>
2022-01-08 14:46:16 -0500

    Better docs

--- src/rate-limiter.ts
+++ src/rate-limiter.ts
@@ -3,11 +3,21 @@ import { RateLimiter as Limiter } from 'limiter';
 export class RateLimiter {
     private limiters: { [key: string]: Limiter } = {};
 
+    /**
+     * Creates a new RateLimiter object to control rate limiting.
+     *
+     * @param amount - Amount of times an action can be done within an interval. Ex: `2` would mean 2 times.
+     * @param interval - Length of an interval in milliseconds. Ex: `5000` would mean 5 seconds.
+     *
+     * @returns RateLimiter object.
+     */
     constructor(public amount: number, public interval: number) {}
 
     /**
      * Takes a token from the rate limiter.
-     * @param key A key which identifies the entity being limited (Ex: a username or ID).
+     *
+     * @param key Key which identifies the entity being limited (Ex: a username or ID).
+     *
      * @returns Whether this action exceeds the rate limit.
      */
     public take(key: string): boolean {
