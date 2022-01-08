e314111a9298bc7cb136a1be93394ee1b082c92c
Kevin Novak <kevinmichaelnovak@gmail.com>
2022-01-08 14:45:05 -0500

    Better docs

--- src/rate-limiter.ts
+++ src/rate-limiter.ts
@@ -6,11 +6,11 @@ type LimitResult = {
      */
     limited: boolean;
     /**
-     * The current interval start time.
+     * Current interval start time in milliseconds.
      */
     start: number;
     /**
-     * The current interval end time.
+     * Current interval end time in milliseconds.
      */
     end: number;
 };
@@ -18,12 +18,22 @@ type LimitResult = {
 export class RateLimiter {
     private limiters: { [key: string]: Limiter } = {};
 
+    /**
+     * Creates a new RateLimiter object to control rate limiting.
+     *
+     * @param amount - Amount of times an action can be done within an interval. Ex: `2` would mean 2 times.
+     * @param interval - Length of an interval in milliseconds. Ex: `5000` would mean 5 seconds.
+     *
+     * @returns RateLimiter object.
+     */
     constructor(public amount: number, public interval: number) {}
 
     /**
      * Takes a token from the rate limiter.
-     * @param key A key which identifies the entity being limited (Ex: a username or ID).
-     * @returns The result of performing this action.
+     *
+     * @param key Key which identifies the entity being limited (Ex: a username or ID).
+     *
+     * @returns Result of performing this action.
      */
     public take(key: string): LimitResult {
         let limiter = this.limiters[key];
